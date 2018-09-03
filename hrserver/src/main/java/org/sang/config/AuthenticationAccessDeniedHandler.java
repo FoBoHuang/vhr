1f7d1eeac891866ea27391c32d75077a7b89a3a2
marvHuang <hfb161716@163.com>
2018-09-03 22:39:31 +0800

    [add]自定义403的响应内容

--- hrserver/src/main/java/org/sang/config/AuthenticationAccessDeniedHandler.java
+++ hrserver/src/main/java/org/sang/config/AuthenticationAccessDeniedHandler.java
@@ -15,6 +15,15 @@ import java.io.PrintWriter;
  */
 @Component
 public class AuthenticationAccessDeniedHandler implements AccessDeniedHandler {
+
+    /*/*
+     * @功能描述 自定义AccessDeniedHandler我们可以自定义403响应的内容
+     *
+     * @author Huang
+     * @date 2018/9/3 22:38
+     * @param [httpServletRequest, resp, e] 
+     * @return void
+     */
     @Override
     public void handle(HttpServletRequest httpServletRequest, HttpServletResponse resp, AccessDeniedException e) throws IOException, ServletException {
         resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
